e157dba4fee8404916e3b99df9fc153ff3fb4644
roberto flores <robertoflores2790@gmail.com>
2019-02-03 12:39:58 -0600

    build a person class with his respective properties

---
+++ PersonsAPI/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonsAPI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public bool Disabled { get; set; }
+
+    }
+}
