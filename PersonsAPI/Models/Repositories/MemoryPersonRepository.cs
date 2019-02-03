8e14173c711d5ea42239dd89835060112f738750
roberto flores <robertoflores2790@gmail.com>
2019-02-03 17:14:57 -0600

    get all persons, pagination and cache

--- PersonsAPI/Models/Repositories/MemoryPersonRepository.cs
+++ PersonsAPI/Models/Repositories/MemoryPersonRepository.cs
@@ -1,4 +1,6 @@
-﻿using PersonsAPI.Models.Repositories.Interfaces;
+﻿using Microsoft.Extensions.Caching.Memory;
+using PersonsAPI.Models.Entity;
+using PersonsAPI.Models.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +8,28 @@ using System.Threading.Tasks;
 
 namespace PersonsAPI.Models.Repositories
 {
-    public class MemoryPersonRepository: IPersonRepository
+    public class MemoryPersonRepository 
     {
+        public IList<Person> personList { get; set; }
+        public MemoryPersonRepository(IMemoryCache cache)
+        {
 
+            personList = new PersonCacheService(cache).GetPersons();
+        }
+
+        public IList<Person> GetAll(int skip, int take)
+        {
+            IList<Person> response;
+            if(skip > 0 || take > 0)
+            {
+                response = personList.AsQueryable().Skip(skip).Take(take).ToList();
+            }
+            else
+            {
+                response = personList;
+            }
+            return response;
+          
+        }
     }
 }
---
+++ PersonsAPI/Models/Services/PersonCacheService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using PersonsAPI.Models.Entity;
+using PersonsAPI.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonsAPI.Models.Services
+{
+    public class PersonCacheService
+    {
+        public IList<Person> _items { get; set; }
+        private const string _cacheKey = "cache-key";
+
+        private readonly IMemoryCache _cache;
+
+
+        public PersonCacheService(IMemoryCache cache)
+        {
+            _cache = cache;
+
+        }
+
+        public IList<Person> GetPersons()
+        {
+            
+            if (_cache.TryGetValue(_cacheKey, out IList<Person> persons))
+            {
+                return persons;
+            }
+
+            _cache.Set(_cacheKey, fillPersons());
+            if (_cache.TryGetValue(_cacheKey, out IList<Person> personsCached))
+            {
+                return personsCached;
+            }
+
+            return persons;
+
+        }
+
+        public List<Person> fillPersons()
+        {
+           
+           var persons = new List<Person>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var basePerson = new Person()
+                {
+                    Id = i,
+                    FistName = "Roberto",
+                    LastName = "Flores",
+                    Disabled = ((i & 1) == 0)
+                };
+
+                persons.Add(basePerson);
+            }
+            return persons;
+        }
+    }
+}
